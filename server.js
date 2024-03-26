cad21e46922f3e457d20c005db543c1ea485c8f1
Patrick Bowen <patrickrbowen28@gmail.com>
2024-03-25 18:41:12 -0700

    first commit user login

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const userRoutes = require('./routes/userRoutes');
+const sequelize = require('./config/database');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+sequelize.sync()
+    .then(() => {
+        console.log('Database is synched!')
+    })
+    .catch((error) => {
+        console.error('unable to sync!', error);
+    })
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
