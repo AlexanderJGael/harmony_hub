f3ebecd64965029d4f283cca66838b703daa7eaa
Patrick Bowen <patrickrbowen28@gmail.com>
2024-03-28 18:04:52 -0700

    front end login, registration

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const dotenv = require('dotenv');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const authRoutes = require('./routes/authRoutes');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
@@ -33,7 +32,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/auth', authRoutes);
 
 sequelize.sync()
     .then(() => {
