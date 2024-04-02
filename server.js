5844ec09bd37ca5a48314f911629490d4786b5bb
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-04-01 21:18:57 -0500

    chat functioning

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const dotenv = require('dotenv');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
-const userRoutes = require('./routes/userRoutes');
+// const userRoutes = require('./routes/userRoutes');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -16,6 +18,11 @@ const { Messages, User } = require('./config/database');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {}
+});
 
 const hbs = exphbs.create({ helpers });
 
@@ -35,9 +42,12 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/users', userRoutes);
+// app.use(routes)
 
 sequelize.sync().then(() => {
+  server.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
   console.log(sequelize.models);
   console.log('Database synced');
 })
@@ -47,11 +57,6 @@ sequelize.sync().then(() => {
 
 const main = async () => {
   const Messages = sequelize.models.Messages;
-
-  const server = createServer(app);
-  const io = new Server(server, {
-    connectionStateRecovery: {}
-  });
 
   // socket.io
   io.on('connection', (socket) => {
@@ -97,10 +102,6 @@ const main = async () => {
         io.emit('chat message', lastMessage.content);
       };
     });
-  });
-
-  server.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
   });
 };
 
