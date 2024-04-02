54c9f8df99e3bf5f5c645dc0877427f77a608d42
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-04-01 21:24:19 -0500

    GET fix

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(routes)
+app.use(routes)
 
 sequelize.sync().then(() => {
   server.listen(PORT, () => {
