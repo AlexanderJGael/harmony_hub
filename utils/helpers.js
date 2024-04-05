60fb793ff77fff80e108b333321daa6ff756f38f
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-04-04 22:08:45 -0500

    test heroku deployment

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,4 @@
-const Message = require('../models/Message');
+const Message = require('../models/message');
 const User = require('../models/User');
 const { Sequelize } = require('sequelize');
 const bcrypt = require('bcrypt');
