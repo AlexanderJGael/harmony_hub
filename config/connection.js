7d6e8d203fcd0fa5de2279a8bcfab10f55aca9bc
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-04-09 05:22:37 -0500

    close idle session

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,13 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306
+        port: 3306,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+          },
     });
 }
 
