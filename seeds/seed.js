a02aa750fb04dfb7894c59236156e9b1ecdd4c91
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-04-10 14:22:54 -0500

    fixed UniqueContraintError

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,14 +3,48 @@ const { User, Messages, Profile, Forum, Blog } = require('../models');
 const userData = require("./userData.json");
 const forumData = require("./forumData.json");
 const blogData = require("./blogData.json");
+const { Sequelize } = require('sequelize');
 
 const seedDatabase = async (userData, forumData, blogData) => {
-    await sequelize.sync();
+    await sequelize.sync({ force: false })
 
-    await User.bulkCreate(userData, {
-      individualHooks: true,
-      returning: true,
-    });
+    for (const user of userData) {
+      try {
+        await User.create(user);
+      } catch (e) {
+        if (e.name !== 'SequelizeUniqueConstraintError') {
+          throw e;
+        }
+      }
+    }
+
+    for (const forum of forumData) {
+      try {
+        const author = await User.findOne({ where: { username: forum.authorName } });
+        await Forum.create({
+          ...forum,
+          authorId: author.id
+        });
+      } catch (e) {
+        if (e.name !== 'SequelizeUniqueConstraintError') {
+          throw e;
+        }
+      }
+    }
+
+    for (const blog of blogData) {
+      try {
+        const author = await User.findOne({ where: { username: blog.authorName } });
+        await Blog.create({
+          ...blog,
+          authorId: author.id
+        });
+      } catch (e) {
+        if (e.name !== 'SequelizeUniqueConstraintError') {
+          throw e;
+        }
+      }
+    }
 
     console.log("Seeding complete!");
     process.exit(0);
